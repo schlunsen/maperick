2169ddc6ed24cf5845da367b126b88d235b6520c
Rasmus Schlunsen <rasmus@schlunsen.com>
2022-04-27 23:01:37 +0200

    Switch from argh to clap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use clap::{Arg, Command};
 
 mod app;
 #[cfg(feature = "crossterm")]
@@ -11,23 +12,34 @@ mod ui;
 use crate::crossterm::run;
 #[cfg(feature = "termion")]
 use crate::termion::run;
-use argh::FromArgs;
+use std::default;
 use std::{error::Error, time::Duration};
 
-/// Demo
-#[derive(Debug, FromArgs)]
-struct Cli {
+
+
+/// Map tcp connections on a TUI world map
+#[derive(Parser, Debug)]
+#[clap(name = "maperick")]
+#[clap(version = "0.1")]
+#[clap(about, long_about = None)]
+struct Args {
     /// time in ms between two ticks.
-    #[argh(option, default = "250")]
-    tick_rate: u64,
+    #[clap(takes_value = false)]
+    #[clap(help = "time in ms between two ticks.")]
+    #[clap(short = 't', default_missing_value = "250")]
+    tick_rate: Option<u64>,
+
     /// whether unicode symbols are used to improve the overall look of the app
-    #[argh(option, default = "true")]
+    #[clap(short = 'e', default_missing_value = "true",)]
     enhanced_graphics: bool,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let cli: Cli = argh::from_env();
-    let tick_rate = Duration::from_millis(cli.tick_rate);
-    run(tick_rate, cli.enhanced_graphics)?;
+
+    let args =  Args::parse();
+
+    let tick_rate = if (args.tick_rate.unwrap_or(0) > 0) { Duration::from_millis(args.tick_rate.expect("")) } else { Duration::from_millis(250) };
+    
+    run(tick_rate, args.enhanced_graphics)?;
     Ok(())
 }
